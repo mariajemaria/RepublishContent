4bb70a811f066a099d18055133f9b6afb090f7f1
Marija Jemuovic <marija.jemuovic@gmail.com>
2015-02-10 11:25:59 +0100

    Republish previously published content (EPiServer Admin Tool)
    This is a simple tool that goes through all content items and publishes
    them (if they are not pending published or their StartPublish date is
    set to future)

---
+++ EPiServerUI/Admin/Tools/RepublishPreviouslyPublishedContent.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="RepublishPreviouslyPublishedContent.aspx.cs" Inherits="PROJECT.Web.EPiServerUI.Admin.Tools.RepublishPreviouslyPublishedContent" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Republish previously published content</title>
+
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label runat="server" ID="doneLabel" Text="Done!" Visible="false" />
+            <p>
+                <strong>Note:</strong> This job publishes goes through the whole tree and republishes all pages (that were previously published).
+                <br/>
+            </p>
+            
+            <p>Skip the following content types:</p>
+            
+            <input type="checkbox" class="js-toggle-all-page-types" value="Toggle all/none" /> <strong>Toggle all/none</strong>
+            <asp:CheckBoxList runat="server" ID="skippedContentCheckBoxList" CssClass="js-skipped-page-types" />
+            <br />
+            <asp:Button runat="server" ID="doRepublishButton" OnClick="doRepublishButton_OnClick" Text="Re-publish all" />
+
+        </div>
+    </form>
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $(".js-toggle-all-page-types").change(function () {
+                var allChecked = $(this).is(':checked');
+                var allCheckboxes = $(".js-skipped-page-types input:checkbox");
+                $(allCheckboxes).prop("checked", allChecked);
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ EPiServerUI/Admin/Tools/RepublishPreviouslyPublishedContent.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Data.Entity;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAccess;
+using EPiServer.PlugIn;
+using EPiServer.ServiceLocation;
+
+namespace PROJECT.EPiServerUI.Admin.Tools
+{
+    [GuiPlugIn(
+        DisplayName = "Re-publish content",
+        Description = "Goes through the whole tree starting from the root and republishes all content (that was previously published)",
+        Area = PlugInArea.AdminMenu,
+        Url = "~/EPiServerUI/Admin/Tools/RepublishPreviouslyPublishedContent.aspx")]
+    public partial class RepublishPreviouslyPublishedContent : Page
+    {
+        private IContentRepository _contentRepository;
+        private IContentTypeRepository _contentTypeRepository;
+        
+        protected override void OnInit(EventArgs e)
+        {
+            EnableViewState = false;
+
+            _contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
+            _contentTypeRepository = ServiceLocator.Current.GetInstance<IContentTypeRepository>();
+
+            skippedContentCheckBoxList.Items.Clear();
+
+            var alphabeticalContentItems = _contentTypeRepository.List().OrderBy(t => t.LocalizedName).ToList();
+
+            foreach (var contentType in alphabeticalContentItems)
+            {
+                var checkboxItem = new ListItem(contentType.LocalizedName, contentType.ID.ToString(CultureInfo.InvariantCulture));
+                skippedContentCheckBoxList.Items.Add(checkboxItem);
+            }
+
+            base.OnInit(e);
+        }
+
+        protected void doRepublishButton_OnClick(object sender, EventArgs e)
+        {
+            var skippedContentTypeIds = new List<int>();
+
+            foreach (ListItem contentType in skippedContentCheckBoxList.Items)
+            {
+                if (contentType.Selected)
+                {
+                    skippedContentTypeIds.Add(int.Parse(contentType.Value));
+                }
+            }
+
+            RepublishContentAndChildren(ContentReference.RootPage, skippedContentTypeIds);
+
+            doneLabel.Visible = true;
+        }
+
+        private void RepublishContentAndChildren(ContentReference reference, List<int> skippedContentTypeIds)
+        {
+            IContent content;
+            _contentRepository.TryGet(reference, out content);
+            var versionable = content as IVersionable;
+
+            if (versionable != null &&
+                versionable.StartPublish <= DateTime.Now &&
+                !versionable.IsPendingPublish &&
+                !skippedContentTypeIds.Contains(content.ContentTypeID))
+            {
+                try
+                {
+                    var clone = ((IReadOnly)content).CreateWritableClone() as IContent;
+								
+                    // this is the place where you do "something" with each of the content items you are going through
+
+                    _contentRepository.Save(clone, SaveAction.Publish);
+                }
+                catch (Exception ex)
+                {
+                    // add logging here
+                }
+            }
+
+            // don't publish content from recycle bin
+            if (reference != ContentReference.WasteBasket)
+            {
+                var children = _contentRepository.GetChildren<IContent>(reference).ToList();
+                foreach (var child in children)
+                {
+                    RepublishContentAndChildren(child.ContentLink, skippedContentTypeIds);
+                }
+            }
+        }
+    }
+}
